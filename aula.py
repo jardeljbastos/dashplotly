dda0d3a17897e30ec94ebda0db7adc2c2475fccd
Jardel Bastos <84300151+jardeljbastos@users.noreply.github.com>
2024-10-27 19:18:20 -0300

    Update aula.py claude

--- aula.py
+++ aula.py
@@ -1,30 +1,27 @@
-from dash import dash, html, dcc, Input, Output
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
-app = app.Dash(__name__)
-server = app.server
-#app = Dash(__dash__)
+# Inicialização do app - aqui estava o primeiro erro
+app = Dash(__name__)  # Era app.Dash que está incorreto
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+# Leitura do arquivo
 df = pd.read_excel("Vendas.xlsx")
 
-# criando o gráfico
+# Criando o gráfico
 fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+
+# Criando lista de opções para o dropdown
 opcoes = list(df['ID Loja'].unique())
 opcoes.append("Todas as lojas")
 
 app.layout = html.Div(children=[
     html.H1(children='Faturamento das Lojas'),
     html.H2(children='Gráfico com o faturamento de Todos os Produtos separados por Loja'),
-
     html.Div(children='''
         Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
     '''),
-
     dcc.Dropdown(opcoes, value='Todas as lojas', id='lista_lojas'),
-
     dcc.Graph(
         id='grafico_quantidade_vendas',
         figure=fig
@@ -43,5 +40,9 @@ def update_output(value):
         fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
     return fig
 
+# Definição do server para o Gunicorn
+server = app.server
+
+# Correção na condição if __name__ (estava com ** ao invés de __)
 if __name__ == '__main__':
     app.run(debug=True)
