57898525b969274dc119efc222452e70d80ca2f3
Jardel Bastos <84300151+jardeljbastos@users.noreply.github.com>
2024-10-27 21:20:42 -0300

    Update aula.py find
    Editado grafico sexo find

--- aula.py
+++ aula.py
@@ -2,14 +2,16 @@ from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
-# Inicialização do app - aqui estava o primeiro erro
-app = Dash(__name__)  # Era app.Dash que está incorreto
+# Inicialização do app
+app = Dash(__name__)
+
+# Configurar o servidor
+server = app.server
 
 # Leitura do arquivo
 df = pd.read_excel("ENEMDados2023.xls")
 
 # Criando o gráfico
-#fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
 fig = px.bar(
     df['TP_SEXO'].value_counts().reset_index(),
     x='index',
@@ -23,7 +25,6 @@ fig = px.bar(
 # Criando lista de opções para o dropdown
 opcoes = list(df['TP_SEXO'].unique())
 opcoes.append("Todos os Sexos")
-#opcoes.append("Todas as lojas")
 
 fig.update_traces(textposition='outside')
 fig.update_layout(
@@ -37,21 +38,12 @@ app.layout = html.Div(children=[
             style={'textAlign': 'center', 'color': '#503D36', 'marginBottom': 30}
            ),
     
-#    html.H2(children='Gráfico com o faturamento de Todos os Produtos separados por Loja'),
-#    html.Div(children='''
-#        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
-#    '''),
     html.Div(children='''
         Distribuição dos candidatos por sexo no ENEM 2023
     ''', style={'textAlign': 'center', 'marginBottom': 30}),
 
-#    dcc.Dropdown(opcoes, value='Todas as lojas', id='lista_lojas'),
     dcc.Dropdown(opcoes, value='Todos os Sexos', id='lista_sexo'),
-    dcc.Graph(
-#        id='grafico_quantidade_vendas',
-        id='grafico_sexo',
-        figure=fig
-    )
+    dcc.Graph(id='grafico_sexo', figure=fig)
 ])
 
 @app.callback(
@@ -59,21 +51,14 @@ app.layout = html.Div(children=[
     Input('lista_sexo', 'value')
 )
 def update_output(value):
-#    if value == "Todas as lojas":
     if value == "Todos os Sexos":
-#        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
-##        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
         fig = px.bar(df['TP_SEXO'].value_counts().reset_index(), x='index', y='TP_SEXO', title='Distribuição de Candidatos por Sexo - ENEM 2023', labels={'index': 'Sexo', 'TP_SEXO': 'Número de Candidatos'}, color='index', text='TP_SEXO')
     else:
-#        tabela_filtrada = df.loc[df['ID Loja']==value, :]
-        tabela_filtrada = df.loc[df['TP_SEXO']==value, :]
-#        fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
-        fig = px.bar(tabela_filtrada, df['TP_SEXO'].value_counts().reset_index(), x='index', y='TP_SEXO', title='Distribuição de Candidatos por Sexo - ENEM 2023', labels={'index': 'Sexo', 'TP_SEXO': 'Número de Candidatos'}, color='index', text='TP_SEXO')
+        tabela_filtrada = df.loc[df['TP_SEXO'] == value, :]
+        fig = px.bar(tabela_filtrada, x='TP_SEXO', y='TP_SEXO', title=f'Distribuição de {value} no ENEM 2023', labels={'x': 'Candidato', 'y': 'Número de Candidatos'}, color='TP_SEXO', text='TP_SEXO')
 
     return fig
 
-# Definição do server para o Gunicorn não alterar a partir daqui
-server = app.server
-
+# Para o deploy no Render, remova ou comente o seguinte bloco
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
