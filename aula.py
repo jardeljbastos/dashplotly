cdfbfb2725e18d8e2460bbe77e1713ae6c8034f6
Jardel Bastos <84300151+jardeljbastos@users.noreply.github.com>
2024-10-27 21:04:19 -0300

    Update aula.py grafico 1
    editando grafico sexo base ENEM

--- aula.py
+++ aula.py
@@ -6,43 +6,74 @@ import pandas as pd
 app = Dash(__name__)  # Era app.Dash que está incorreto
 
 # Leitura do arquivo
-df = pd.read_excel("Vendas.xlsx")
+df = pd.read_excel("ENEMDados2023.xls")
 
 # Criando o gráfico
-fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+#fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+fig = px.bar(
+    df['TP_SEXO'].value_counts().reset_index(),
+    x='index',
+    y='TP_SEXO',
+    title='Distribuição de Candidatos por Sexo - ENEM 2023',
+    labels={'index': 'Sexo', 'TP_SEXO': 'Número de Candidatos'},
+    color='index',
+    text='TP_SEXO'
+)
 
 # Criando lista de opções para o dropdown
-opcoes = list(df['ID Loja'].unique())
-opcoes.append("Todas as lojas")
+opcoes = list(df['TP_SEXO'].unique())
+opcoes.append("Todos os Sexos")
+#opcoes.append("Todas as lojas")
+
+fig.update_traces(textposition='outside')
+fig.update_layout(
+    xaxis_title="Sexo",
+    yaxis_title="Número de Candidatos",
+    showlegend=False
+)
 
 app.layout = html.Div(children=[
-    html.H1(children='Faturamento das Lojas'),
-    html.H2(children='Gráfico com o faturamento de Todos os Produtos separados por Loja'),
+    html.H1(children='Análise de Gênero dos Candidatos do ENEM 2023',
+            style={'textAlign': 'center', 'color': '#503D36', 'marginBottom': 30}
+           ),
+    
+#    html.H2(children='Gráfico com o faturamento de Todos os Produtos separados por Loja'),
+#    html.Div(children='''
+#        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
+#    '''),
     html.Div(children='''
-        Obs: Esse gráfico mostra a quantidade de produtos vendidos, não o faturamento.
-    '''),
-    dcc.Dropdown(opcoes, value='Todas as lojas', id='lista_lojas'),
+        Distribuição dos candidatos por sexo no ENEM 2023
+    ''', style={'textAlign': 'center', 'marginBottom': 30}),
+
+#    dcc.Dropdown(opcoes, value='Todas as lojas', id='lista_lojas'),
+    dcc.Dropdown(opcoes, value='Todos os Sexos', id='lista_sexo'),
     dcc.Graph(
-        id='grafico_quantidade_vendas',
+#        id='grafico_quantidade_vendas',
+        id='grafico_sexo',
         figure=fig
     )
 ])
 
 @app.callback(
-    Output('grafico_quantidade_vendas', 'figure'),
-    Input('lista_lojas', 'value')
+    Output('grafico_sexo', 'figure'),
+    Input('lista_sexo', 'value')
 )
 def update_output(value):
-    if value == "Todas as lojas":
-        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+#    if value == "Todas as lojas":
+    if value == "Todos os Sexos":
+#        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+##        fig = px.bar(df, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig = px.bar(df['TP_SEXO'].value_counts().reset_index(), x='index', y='TP_SEXO', title='Distribuição de Candidatos por Sexo - ENEM 2023', labels={'index': 'Sexo', 'TP_SEXO': 'Número de Candidatos'}, color='index', text='TP_SEXO')
     else:
-        tabela_filtrada = df.loc[df['ID Loja']==value, :]
-        fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+#        tabela_filtrada = df.loc[df['ID Loja']==value, :]
+        tabela_filtrada = df.loc[df['TP_SEXO']==value, :]
+#        fig = px.bar(tabela_filtrada, x="Produto", y="Quantidade", color="ID Loja", barmode="group")
+        fig = px.bar(tabela_filtrada, df['TP_SEXO'].value_counts().reset_index(), x='index', y='TP_SEXO', title='Distribuição de Candidatos por Sexo - ENEM 2023', labels={'index': 'Sexo', 'TP_SEXO': 'Número de Candidatos'}, color='index', text='TP_SEXO')
+
     return fig
 
-# Definição do server para o Gunicorn
+# Definição do server para o Gunicorn não alterar a partir daqui
 server = app.server
 
-# Correção na condição if __name__ (estava com ** ao invés de __)
 if __name__ == '__main__':
     app.run(debug=True)
